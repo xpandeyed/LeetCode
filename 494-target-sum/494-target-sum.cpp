eaaa11aee67568adeb4d0e0a482c01023e75194c
Lal Bihari Pandey <74754285+xpandeyed@users.noreply.github.com>
2022-01-21 11:26:38 +0530

    Time: 38 ms (53.48%), Space: 12.5 MB (38.74%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -1,22 +1,48 @@
 class Solution {
 public:
-    int helper(vector<int>& nums, int target, int index, int currSum){
-        if(index==0){
-            if(currSum+nums[0]==target && currSum-nums[0]==target){
-                return 2;
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int n=nums.size(), i, j, total=0, sum, count=0, mi=nums[0];
+        for(i=0;i<n; ++i){
+            total+=nums[i];
+            if(nums[i]==0){
+                count++;
             }
-            if(currSum+nums[0]==target || currSum-nums[0]==target){
-                return 1;
-            }
-            else{
-                return 0;
+            if(nums[i]<mi){
+                mi=nums[i];
             }
         }
-        return helper(nums, target, index-1, currSum+nums[index]) + helper(nums, target, index-1, currSum-nums[index]);
-    }
-    
-    
-    int findTargetSumWays(vector<int>& nums, int target) {
-        return helper(nums, target, nums.size()-1, 0);
+        if(target>total){
+            return 0;
+        }
+        sum = (total+target)/2;
+        if((total+target)%2!=0){
+            return 0;
+        }
+        if(sum<-1){
+            return 0;
+        }
+        vector<vector<int>> res(n+1, vector<int>(sum+1, 0));
+        for(i=0; i<n+1;++i ){
+            for(j=0; j<sum+1; ++j){
+                if(j==0){
+                    res[i][j]=1;
+                    continue;
+                }
+                if(i==0){
+                    res[i][j]=0;
+                    continue;
+                }
+                if(nums[i-1]==0){
+                    res[i][j]=res[i-1][j];
+                    continue;
+                }
+                if(nums[i-1]>j){
+                    res[i][j]=res[i-1][j];
+                }else{
+                    res[i][j] = res[i-1][j]+res[i-1][j-nums[i-1]];
+                }
+            }
+        }
+        return res[n][sum]*pow(2, count);
     }
 };
