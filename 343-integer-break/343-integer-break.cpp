1c986126c4f61fedb61f408e71e95432dff8d36d
Lal Bihari Pandey <74754285+xpandeyed@users.noreply.github.com>
2022-01-23 21:21:36 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (25.86%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,29 +1,15 @@
 class Solution {
 public:
-    
-    int helper(int n, vector<int> &results){
-        if(n==1){
-            return results[1] = 1;
-        }
-        if(results[n]!=-1){
-            return results[n];
-        }
-        int res , i ;
-        res=n-1;
-        for(i=2; i<=n/2; ++i){
-            int res1 = i*(n-i);
-            int res2 = i*helper(n-i, results);
-            int res3 = helper(i, results)*(n-i);
-            res = max(res, max(res1, max(res2, res3)));
-        }
-        return results[n] = res;
-        
-    }
-    
-    
     int integerBreak(int n) {
-        vector<int> res(n+1, -1);
-        helper(n, res);
+        vector<int> res(n+1, 1);
+        res[0]=0;
+        int i, j, temp;
+        for(i=2; i<=n; ++i){
+            for(j=1; j<=i/2; ++j){
+                temp = max(max(j*res[i-j], j*(i-j)), max(res[j]*(i-j), res[j]*res[i-j]));
+                res[i]=max(res[i], temp);
+            }
+        }
         return res[n];
     }
 };
