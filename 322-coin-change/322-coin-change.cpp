b42b6303a669f37bb18d3a3a682e83637ca2c03b
Lal Bihari Pandey <74754285+xpandeyed@users.noreply.github.com>
2022-01-25 11:23:13 +0530

    Time: 308 ms (8.52%), Space: 49.2 MB (10.60%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
-    int memo(vector<int>& coins, int amount, int items, vector<vector<int>> &res){
-        if(items==0 && amount==0){
-            return res[items][amount] = 0;
-        } 
-        if(items==0 && amount!=0){
-            return res[items][amount] = -1;
-        }
-        if(amount==0){
-            return res[items][amount] = 0;
-        }
-        if(res[items][amount]!=-2){
-            return res[items][amount];
-        }
-        if(coins[items-1]>amount){
-            return res[items][amount] = memo(coins, amount, items-1, res);
-        }else{
-            int a = memo(coins, amount, items-1, res);
-            int b = memo(coins, amount-coins[items-1], items, res);
-            if(a==-1 && b==-1){
-                return res[items][amount] = -1;
+    int coinChange(vector<int>& coins, int amount) {
+            int n=coins.size(), i, j, a, b;
+    vector<vector<int>> res(n+1, vector<int>(amount+1, -1));
+    for(i=0; i<n+1; ++i){
+        for(j=0; j<amount+1; ++j){
+            if(j==0){
+                res[i][j]=0;
+                continue;
             }
-            if(a==-1){
-                return res[items][amount] = b+1;
+            if(i==0){
+                res[i][j]=-1;
+                continue;
             }
-            if(b==-1){
-                return res[items][amount] = a;
+            if(coins[i-1]>j){
+                res[i][j]=res[i-1][j];
+            }else{
+                a = res[i-1][j];
+                b = res[i][j-coins[i-1]];
+                if(a==-1 && b==-1){
+                    res[i][j]=-1;
+                    continue;
+                }
+                if(a!=-1 && b==-1){
+                    res[i][j]=a;
+                    continue;
+                }
+                if(a==-1 && b!=-1){
+                    res[i][j]=(b+1);
+                    continue;
+                }
+                if(a!=-1 && b!=-1){
+                    res[i][j] = min(a, b+1);
+                }
             }
-            return res[items][amount] = min(a, b+1);
         }
     }
-    
-    
-    int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>> res(n+1, vector<int>(amount+1, -2));
-        memo(coins, amount, n, res);
-        return res[n][amount];
+    return res[n][amount];
     }
 };
