95dfc4603943cf24d8b1704dec5501ebbe449ed8
Lal Bihari Pandey <74754285+xpandeyed@users.noreply.github.com>
2022-02-20 13:02:31 +0530

    Time: 8 ms (63.69%), Space: 13.9 MB (26.80%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -11,9 +11,20 @@ public:
         return res[i] = cost[i]+min(rec(cost, i+1, n, res), rec(cost, i+2, n, res));
     }
     
+    int dynamic(vector<int> cost){
+        int n=cost.size(), i;
+        for(i=n-3; i>=0; --i){
+            cost[i]+=min(cost[i+1], cost[i+2]);
+        }
+        
+        return min(cost[0], cost[1]);
+    }
+    
     int minCostClimbingStairs(vector<int>& cost) {
-        int n=cost.size();
-        vector<int> res(n+4, -1);
-        return min(rec(cost, 0, n, res),rec(cost, 1, n, res));
+        // int n=cost.size();
+        // vector<int> res(n+4, -1);
+        // return min(rec(cost, 0, n, res),rec(cost, 1, n, res));
+        
+        return dynamic(cost);
     }
 };
