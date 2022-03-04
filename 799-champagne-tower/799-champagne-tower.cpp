3876c6b23efbdb247c90531b54f4e2a00f603e21
Lal Bihari Pandey <74754285+xpandeyed@users.noreply.github.com>
2022-03-04 15:44:45 +0530

    Time: 22 ms (39.24%), Space: 11.1 MB (58.73%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -1,14 +1,43 @@
 class Solution {
 public:
-    double champagneTower(int poured, int query_row, int query_glass) {
-    vector<vector<double>>pascal(query_row+1,vector<double>(query_glass+2,0.0));
-        pascal[0][0]=poured;
-        for(int i=0;i<query_row;i++)
-            for(int j=0;j<=query_glass;j++)
-            {
-                pascal[i+1][j]+=max(0.0,(pascal[i][j]-1)/2.0);
-                pascal[i+1][j+1]+=max(0.0,(pascal[i][j]-1)/2.0);
+    double champagneTower(int poured, int row, int glass) {
+        vector<vector<double>>ch;
+        
+        int i, j;
+        
+        for(i=0; i<=row; ++i){
+            ch.push_back(vector<double>(i+1));
+        }
+        
+        ch[0][0]=poured;
+        
+        double left, right;
+        
+        for(i=1; i<=row; ++i){
+            
+            ch[i][0]=ch[i-1][0]>1.0?((ch[i-1][0]-1.0)/2):0.0;
+            
+            for(j=1; j<i; ++j){
+                ch[i][j]=0.0;
+                left = ch[i-1][j-1];
+                right = ch[i-1][j];
+                if(left>1.0){
+                    ch[i][j]+=(left-1.0)/2;
+                }
+                if(right>1.0){
+                    ch[i][j]+=(right-1.0)/2;
+                }
             }
-        return min(1.0,pascal[query_row][query_glass]);
+            ch[i][j] = ch[i-1][j-1]>1.0?((ch[i-1][j-1]-1.0)/2):0.0;
+        }
+        
+//         for(auto x: ch){
+//             for(auto y: x){
+//                 cout << y << " ";
+//             }
+//             cout << endl;
+//         }
+        
+        return min(1.0,ch[row][glass]);
     }
 };
