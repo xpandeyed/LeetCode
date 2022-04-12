f5d830e1e9eb57203157b8aafcc6109ffae87af8
Lal Bihari Pandey <74754285+xpandeyed@users.noreply.github.com>
2022-04-12 23:00:03 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (58.55%) - LeetHub

--- 289-game-of-life/289-game-of-life.cpp
+++ 289-game-of-life/289-game-of-life.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void gameOfLife(vector<vector<int>>& board) {
         int rows=board.size(), cols=board[0].size(), i, j, live, dead;
-        vector<vector<int>> res(rows, vector<int>(cols, 0));
+        // vector<vector<int>> res(rows, vector<int>(cols, 0));
         for(i=0; i<rows; ++i){
             for(j=0; j<cols; ++j){
                 live=0;
@@ -10,91 +10,106 @@ public:
                 // cout << i << " " << j << endl;
                 //left
                 if(j-1>-1){
-                    if(board[i][j-1]){
+                    if(board[i][j-1]==1 or board[i][j-1]==-5){
                         live++;
                     }else{
                         dead++;
                     }
                 }
-                
+                // cout << "f1" << endl;
                 //right
                 if(j+1<cols){
-                    if(board[i][j+1]){
+                    if(board[i][j+1]==1 or board[i][j+1]==-5){
                         live++;
                     }else{
                         dead++;
                     }
                 }
+                // cout << "f2" << endl;
                 
                 //top
                 if(i-1>-1){
-                    if(board[i-1][j]){
+                    if(board[i-1][j]==1 or board[i-1][j]==-5){
                         live++;
                     }else{
                         dead++;
                     }
                 }
                 
+                // cout << "f3" << endl;
                 //bottom
                 if(i+1<rows){
-                    if(board[i+1][j]){
+                    if(board[i+1][j]==1 or board[i+1][j]==-5){
                         live++;
                     }else{
                         dead++;
                     }
                 }
+                // cout << "f4" << endl;
                 
                 //top-left
                 if(i-1>-1 and j-1>-1){
-                    if(board[i-1][j-1]){
+                    if(board[i-1][j-1]==1 or board[i-1][j-1]==-5){
                         live++;
                     }else{
                         dead++;
                     }
                 }
-                
+                // cout << "f5" << endl;
                 //bottom-left
                 if(i+1<rows and j-1>-1){
-                    if(board[i+1][j-1]){
+                    if(board[i+1][j-1]==1){
                         live++;
                     }else{
                         dead++;
                     }
                 }
+                // cout << "f6" << endl;
                 //top-right
                 if(i-1>-1 and j+1<cols){
-                    if(board[i-1][j+1]){
+                    if(board[i-1][j+1]==1 or board[i-1][j+1]==-5){
                         live++;
                     }else{
                         dead++;
                     }
                 }
+                // cout << "f7" << endl;
                 
                 //bottom-right
-                if(i+1<rows and j+1<cols){
-                    if(board[i+1][j+1]){
+                if(i+1<rows and j+1<cols ){
+                    if(board[i+1][j+1]==1 or board[i+1][j+1]==-5){
                         live++;
                     }else{
                         dead++;
                     }
                 }
+                // cout << "f8" << endl;
                 // cout << "h" << endl;
-                if(live<2){
-                    res[i][j]=0;
-                }else if(board[i][j] and (live==2 or live==3)){
-                    res[i][j]=1;
-                }else if(board[i][j] and live>3){
-                    res[i][j]=0;
+                if(live<2 and board[i][j]==1){
+                    board[i][j]=-5;
+                }else if(board[i][j]==1 and (live==2 or live==3)){
+                    continue;
+                }else if(board[i][j]==1 and live>3){
+                    board[i][j]=-5;
                 }else if(board[i][j]==0 and live==3){
-                    res[i][j]=1;
+                    board[i][j]=5;
                 }
+                // cout << "f9" << endl;
             }
         }
         // cout << "f" <<endl;
         for(i=0; i<rows; ++i){
             for(j=0; j<cols; ++j){
-                board[i][j]=res[i][j];
+                // cout << board[i][j] << " " ;
+                if(board[i][j]==-5){
+                    board[i][j]=0;
+                }
+                if(board[i][j]==5){
+                    board[i][j]=1;
+                }
             }
+            
+            // cout << endl;
         }
     }
 };
