354cc7be634547020dfc35cf28631891e0d56515
Lal Bihari Pandey <74754285+xpandeyed@users.noreply.github.com>
2022-08-19 14:07:41 +0530

    Time: 97 ms (79.29%), Space: 28.2 MB (62.99%) - LeetHub

---
+++ 846-hand-of-straights/846-hand-of-straights.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        map<int, int> f;
+        for(auto x: nums) f[x]++;
+        for(auto x: nums){
+            if(f[x]==0) continue;
+            
+            int i=0;
+            while(i<k){
+                if(f[x+i]==0) return false;
+                f[x+i]--;
+                i++;
+            }
+        }
+        
+        return true;
+    }
+};
